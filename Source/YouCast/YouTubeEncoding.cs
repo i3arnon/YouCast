e902fc3bd1e832dc6616143df75daf85ae6a9eeb
Bar Arnon <i3arnon@gmail.com>
2015-08-28 20:36:06 +0300

    Remove unsupported options

--- Source/YouCast/YouTubeEncoding.cs
+++ Source/YouCast/YouTubeEncoding.cs
@@ -3,10 +3,8 @@ namespace YouCast
 {
     public enum YouTubeEncoding
     {
-        Audio,
-        MP4_360p,
         MP4_720p,
-        MP4_1080p,
-        MP4_3072p,
+        MP4_360p,
+        Audio,
     }
 }
