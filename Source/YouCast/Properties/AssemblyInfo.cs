58618b2d5982bdb601942de2a91ebace154c6320
Bar Arnon <i3arnon@gmail.com>
2015-09-15 20:42:42 +0300

    Project cleanup

--- Source/YouCast/Properties/AssemblyInfo.cs
+++ Source/YouCast/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using YouCast;
 // associated with an assembly.
 
 [assembly: AssemblyTitle(GeneralInformation.ApplicationName)]
-[assembly: AssemblyDescription("YouTube on your terms")]
+[assembly: AssemblyDescription("Turn YouTube Channels into Subscribable Podcasts")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("I3arnon")]
 [assembly: AssemblyProduct(GeneralInformation.ApplicationName)]
@@ -50,5 +50,5 @@ using YouCast;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
