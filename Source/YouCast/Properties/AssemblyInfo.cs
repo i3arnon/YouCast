1944ab15781ce96f391b2b31c1615f22a32a6f05
i3arnon <i3arnon@gmail.com>
2018-01-08 14:25:40 +0200

    Bump version to V2.3

--- Source/YouCast/Properties/AssemblyInfo.cs
+++ Source/YouCast/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using YouCast;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("i3arnon")]
 [assembly: AssemblyProduct(GeneralInformation.ApplicationName)]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,5 +50,5 @@ using YouCast;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
